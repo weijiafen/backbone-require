15c1aa4e82037309abb6b146722972222a31fb68
TammyCT <471636895@qq.com>
2017-09-14 20:46:54 +0800

    Signed-off-by: TammyCT <471636895@qq.com>

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ function proxy(path,response){
 	var opt={
 		host:"www.easy-mock.com",
         path:`/mock/59b8a7b2e0dc663341a7ee9a/test/${path}`
-		// path:`/mock/59b892a7e0dc663341a7b6ee/example/${path}`
 	}
 	var content = '';
 	var req = http.request(opt, function(res) {  
